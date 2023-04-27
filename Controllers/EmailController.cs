c78ce38f78c2ae73e31664d6b4881de62fd10a4f
edsonrosales6 <edsonrosales123@gmail.com>
2023-04-27 13:12:09 -0600

    Se realiza cambio

--- Controllers/EmailController.cs
+++ Controllers/EmailController.cs
@@ -33,23 +33,6 @@ namespace APIPrueba.Controllers
                 context.SendMail.Add(email);
                 context.SaveChanges();
 
-                var mensaje = new MimeMessage();
-                mensaje.From.Add(new MailboxAddress(email.Nombre, "edsonrosales123@gmail.com"));
-                mensaje.To.Add(new MailboxAddress(email.Nombre, email.Correo));
-                mensaje.Subject = email.Nombre + " " + email.Correo;
-                mensaje.Body = new TextPart("plain")
-                {
-                    Text = $"Ciudad y Estado: {email.CiudadYEstado} Telefono: {email.Telefono} Correo: {email.Correo}"
-                };
-
-                using (var cliente = new SmtpClient())
-                {
-                    cliente.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
-                    cliente.Authenticate("edsonrosales123@gmail.com", "edsonrosales123");
-                    cliente.Send(mensaje);
-                    cliente.Disconnect(true);     
-                }
-
                 return Ok();
             } catch(Exception ex) {
                 return BadRequest();
